f2db9458f3c0e02451c260aac97b22d0e8426f40
aokblast <i19780219111@kimo.com>
2022-01-26 19:36:14 +0800

    change mount point from /media to /mnt

--- kernel/cp.sh
+++ kernel/cp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sudo mount ../boot.img /media -t vfat -o loop
-sudo cp kernel.bin /media
-sudo umount /media
+sudo mount ../boot.img /mnt -t vfat -o loop
+sudo cp kernel.bin /mnt
+sudo umount /mnt
